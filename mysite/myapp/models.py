51bc38f20fe97e01b1db2a17293908c664c99da8
damian8896 <damianchng@gmail.com>
2022-07-13 18:01:07 +0800

    django model updates

--- mysite/myapp/models.py
+++ mysite/myapp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=30)
+    descr = models.CharField(max_length=50)
+    date_published = models.DateField(auto_now=False, auto_now_add=False)
+    image = models.ImageField(upload_to='images/')
--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 
+from .models import Article
+
 # Create your views here.
 def index(request):
-    return render(request, "index.html")
+    article = Article.objects.get(pk=1)
+    return render(request, "index.html", {"article":article})
